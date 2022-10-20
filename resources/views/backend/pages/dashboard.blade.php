bd79f114c1eb71617483f8de94beaaf798e2590c
Tifoysal <foysal11203004@gmail.com>
2022-10-20 19:08:26 +0600

    update dashboard

--- resources/views/backend/pages/dashboard.blade.php
+++ resources/views/backend/pages/dashboard.blade.php
@@ -1,7 +1,7 @@
 @extends('backend.master')
 
 @section('content')
-<h1>-------------------hooo</h1>
+<h1>-------------------Hello update</h1>
 <h1>-------------------hooo</h1>
 <h1>-------------------hooo</h1>
 <h1>-------------------hooo</h1>
